d9c4f2e5ab2d18a6dc4e95db8eadf44c9c28fc95
Kumaradoss <76904568+hytrel16@users.noreply.github.com>
2024-08-12 19:23:11 +0530

    Update 2d array.c

--- 2d array.c
+++ 2d array.c
@@ -1,11 +1,22 @@
-//Loop Through a 2D Array
 #include <stdio.h>
 int main(){
    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-   for(int i=0; i<2; i++){
-       for(int j=0; j<3; j++){
-           printf("%d\n", matrix[i][j]);
+   printf("The Matrix: \n");
+   for(int i = 0; i < 2; i++){
+       for(int j = 0; j < 3; j++){
+           printf("%d\t", matrix[i][j]);
        }
+       printf("\n\n");
    }
    return 0;
 }
+
+
+
+/*
+Output:
+The Matrix: 
+1       2       3
+
+4       5       6
+   */
