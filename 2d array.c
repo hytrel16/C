21c9bdcaa8f17bb4ca055478ae88b981c011ceb3
Kumaradoss <76904568+hytrel16@users.noreply.github.com>
2024-08-12 19:01:19 +0530

    Create 2d array.c

---
+++ 2d array.c
@@ -0,0 +1,11 @@
+//Loop Through a 2D Array
+#include <stdio.h>
+int main(){
+   int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+   for(int i=0; i<2; i++){
+       for(int j=0; j<3; j++){
+           printf("%d\n", matrix[i][j]);
+       }
+   }
+   return 0;
+}
