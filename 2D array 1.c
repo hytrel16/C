2903052535d5d100e65969ec32876efffeebfe77
Kumaradoss <76904568+hytrel16@users.noreply.github.com>
2024-08-12 19:19:41 +0530

    Update 2D array 1.c

--- 2D array 1.c
+++ 2D array 1.c
@@ -2,24 +2,38 @@
 #include <stdio.h>
 int main(){
     int row, col;
-    printf("Number of row :");
+    printf("Number of row : ");
     scanf("%d", &row);
-    printf("Number of column :");
+    printf("Number of column : ");
     scanf("%d", &col);
     int matrix[row][col];
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
             printf("Enter the number [%d][%d] :", i, j);
             scanf("%d", &matrix[i][j]);
         }
     }
-    printf("The matrix: ");
-    for(int i=0; i<row; i++){
-       for(int j=0; j<col; j++){
-           printf("%d\n", matrix[i][j]);
+    printf("The matrix: \n");
+    for(int i = 0; i < row; i++){
+       for(int j = 0; j < col; j++){
+           printf("%d\t", matrix[i][j]);
         }
         printf("\n");
     }
     return 0;
-   
 }
+
+/*
+output:
+Number of row : 2
+Number of column : 3
+Enter the number [0][0] :1
+Enter the number [0][1] :2
+Enter the number [0][2] :3
+Enter the number [1][0] :4
+Enter the number [1][1] :5
+Enter the number [1][2] :6
+The matrix: 
+1       2       3
+4       5       6
+*/
